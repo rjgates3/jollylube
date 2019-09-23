9535598aa7f9ddd63d9b39c5b33aa64b3eec2684
rjgates3 <gates.j.richard@gmail.com>
2019-09-23 11:52:17 -0600

    fixed typo

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
     PORT: process.env.PORT || 8000,
     API_TOKEN: process.env.REACT_APP_API_KEY,
-    API_BASE_URL: "http://localhost:8000/api" || process.env.REACT_APP_API_BASE_URL
+    API_BASE_URL:  process.env.REACT_APP_API_BASE_URL
 }
 
-//  
+//  "http://localhost:8000/api" ||
 
